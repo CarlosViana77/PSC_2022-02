733666b9d6169604d8a7e611436b32b1c9d38080
CarlosViana77 <carloseduardovpereira@gmail.com>
2022-08-19 11:39:16 -0300

    Criação de helloworld

---
+++ JavaApplication1/src/javaapplication1/helloworld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 822157798
+ */
+public class helloworld {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+                   
+}}
